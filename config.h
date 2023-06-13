d96fbd66c70013ad2736b2aae5d49666b2b1e3f0
Gustavo <gustavo@fedorabox>
2023-06-13 18:53:21 +0100

    changes

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, " [%s]",           "%F %T" },
-	{ ram_perc, " [RAM %s\%]",     NULL    },
-	{ keymap,   " [%s]",           NULL     },
+	{ datetime, " [%s]"      ,     "%F %T"     },
+	{ ram_perc, " [RAM %s\%]",     NULL        },
+	{ keymap,   " [%s]"      ,     NULL        },
 };
